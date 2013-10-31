abf78a3807c13649bc6467ba9d7abf0117df80e3
Steffion <stefdegoey@gmail.com>
2013-10-31 21:24:28 +0100

    - Removed unused comments.

--- src/nl/Steffion/PLUGINNAME/PLUGINNAME.java
+++ src/nl/Steffion/PLUGINNAME/PLUGINNAME.java
@@ -35,43 +35,6 @@ public class PLUGINNAME extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 		SteffionsEngine.onEnable(this);
 
-		// CMD = new CommandM("PLUGINNAME", "PLUGINNAME", null, null,
-		// Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), null);
-		// CMDinfo = new CommandM("PLUGINNAME INFO", "PLUGINNAME", "info", "i",
-		// Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME [info|i]");
-		// CMDhelp = new CommandM("PLUGINNAME HELP", "PLUGINNAME", "help", "h",
-		// Permissions.help, ConfigC.help_help,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_help),
-		// new CMDhelp(), "/PLUGINNAME <help|h> [page number]");
-		// CMDreload = new CommandM("PLUGINNAME RELOAD", "PLUGINNAME", "reload",
-		// "r", Permissions.reload, ConfigC.help_reload,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_reload),
-		// new CMDreload(), "/PLUGINNAME <reload|r>");
-		// CMDtest1 = new CommandM("PLUGINNAME TESTING", "PLUGINNAME", "test1",
-		// "t1", Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME test1");
-		// CMDtest2 = new CommandM("PLUGINNAME TESTING", "PLUGINNAME", "test2",
-		// "t2", Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME test2");
-		// CMDtest3 = new CommandM("PLUGINNAME TESTING", "PLUGINNAME", "test3",
-		// "t3", Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME test3");
-		// CMDtest4 = new CommandM("PLUGINNAME TESTING", "PLUGINNAME", "test4",
-		// "t4", Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME test4");
-		// CMDtest5 = new CommandM("PLUGINNAME TESTING", "PLUGINNAME", "test5",
-		// "t5", Permissions.info, ConfigC.help_info,
-		// (Boolean) W.config.get(ConfigC.commandEnabled_info),
-		// new CMDinfo(), "/PLUGINNAME test5");
-
 		MessageM.sendFMessage(null, ConfigC.log_enabledPlugin, "name-"
 				+ PLUGINNAME.pdfFile.getName(),
 				"version-" + PLUGINNAME.pdfFile.getVersion(), "autors-"
@@ -144,64 +107,4 @@ public class PLUGINNAME extends JavaPlugin implements Listener {
 		CMDnotfound.exectue(player, cmd, label, args);
 		return true;
 	}
-	
-	// @Override
-	// public boolean onCommand(CommandSender sender, Command cmd, String label,
-	// String[] args) {
-	// Player player = null;
-	// if (sender instanceof Player) {
-	// player = (Player) sender;
-	// }
-	//
-	// for (CommandM command : EW.commands) {
-	// String[] argsSplit = null;
-	// String[] argsSplitAlias = null;
-	//
-	// if (command.args != null && command.argsalias != null) {
-	// argsSplit = command.args.split("/");
-	// argsSplitAlias = command.argsalias.split("/");
-	// }
-	//
-	// if (cmd.getName().equalsIgnoreCase(command.label)) {
-	// boolean equals = true;
-	//
-	// if (argsSplit == null) {
-	// if (args.length == 0) {
-	// equals = true;
-	// } else {
-	// equals = false;
-	// }
-	// } else {
-	// if (args.length >= argsSplit.length) {
-	// for (int i2 = argsSplit.length - 1; i2 >= 0; i2 = i2 - 1) {
-	// int loc = argsSplit.length - i2 - 1;
-	// if (!argsSplit[loc].equalsIgnoreCase(args[loc])
-	// && !argsSplitAlias[loc]
-	// .equalsIgnoreCase(args[loc])) {
-	// equals = false;
-	// }
-	// }
-	// } else {
-	// equals = false;
-	// }
-	// }
-	//
-	// if (equals) {
-	// if (PermissionsM.hasPerm(player, command.permission, true)) {
-	// if (command.enabled) {
-	// command.CMD.exectue(player, cmd, label, args);
-	// } else {
-	// MessageM.sendFMessage(player,
-	// ConfigC.error_commandNotEnabled);
-	// }
-	// }
-	//
-	// return true;
-	// }
-	// }
-	// }
-	//
-	// CMDnotfound.exectue(player, cmd, label, args);
-	// return true;
-	// }
 }
